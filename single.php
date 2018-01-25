f5637f205a8e8cccedc3dc35c012295714ae003d
Joen Asmussen <asmussen@gmail.com>
2018-01-25 13:27:44 +0100

    Various polish
    Column improvements, thumbnail refactor, screenshot, readme.

--- single.php
+++ single.php
@@ -24,14 +24,8 @@
 				$prevPost = get_posts( $args );
 				foreach ( $prevPost as $post ) {
 					setup_postdata( $post );
-					?>
-					<div class="nav-previous<?php if ( has_post_thumbnail() ) { ?> has-post-thumbnail<?php } ?>">
-						<a class="previous" href="<?php the_permalink(); ?>"<?php if ( has_post_thumbnail() ) { ?> style="background-image: url( '<?php echo get_the_post_thumbnail_url( get_the_ID(), 'custom-thumbnails' ) ?>' )"<?php } ?>>
-						<?php the_title( '<h3 class="entry-title">', ' <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="24" height="24"><g><path d="M10 20l8-8-8-8-1.414 1.414L15.172 12l-6.586 6.586"></path></g></svg></h3>' ); ?>
-						</a>
-					</div>
-					<?php
-						wp_reset_postdata();
+					get_template_part( 'template-parts/content', 'thumbnail' );
+					wp_reset_postdata();
 				}
 			}
 
@@ -44,13 +38,7 @@
 				$nextPost = get_posts( $args );
 				foreach ( $nextPost as $post ) {
 					setup_postdata( $post );
-					?>
-					<div class="nav-next<?php if ( has_post_thumbnail() ) { ?> has-post-thumbnail<?php } ?>">
-						<a class="next" href="<?php the_permalink(); ?>"<?php if ( has_post_thumbnail() ) { ?> style="background-image: url( '<?php echo get_the_post_thumbnail_url( get_the_ID(), 'custom-thumbnails' ) ?>' )"<?php } ?>>
-						<?php the_title( '<h3 class="entry-title">', ' <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="24" height="24"><g><path d="M10 20l8-8-8-8-1.414 1.414L15.172 12l-6.586 6.586"></path></g></svg></h3>' ); ?>
-						</a>
-					</div>
-					<?php
+					get_template_part( 'template-parts/content', 'thumbnail' );
 					wp_reset_postdata();
 				}
 			}
