b46a0f7df86fd2296476437036c1e1b74a84ad34
Joen Asmussen <asmussen@gmail.com>
2018-01-26 13:14:54 +0100

    Add missing file.

---
+++ template-parts/content-featured.php
@@ -0,0 +1,7 @@
+<article id="post-<?php the_ID(); ?>" <?php post_class(); ?><?php if ( has_post_thumbnail() ) { ?> style="background-image: url( '<?php echo get_the_post_thumbnail_url( get_the_ID() ) ?>' )"<?php } ?>>
+		<h3><a href="<?php the_permalink(); ?>" rel="bookmark"><?php the_title(); ?></a></h3>
+
+		<div class="entry-meta">
+			<?php navi_posted_on(); ?>
+		</div>
+</article>
