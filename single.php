eb8e804170c522e306c0577132c189da41db19e4
Joen Asmussen <asmussen@gmail.com>
2018-01-26 11:34:14 +0100

    Adjust column system

--- single.php
+++ single.php
@@ -14,35 +14,36 @@
 		<div class="navigation">
 			<h4><strong>Previous</strong> <span class="sep">&</span> <strong>Next</strong></h4>
 			<div class="thumbnails">
-			<?php
-			$prevPost = get_previous_post( true );
-			if ( $prevPost ) {
-				$args = array(
-					'posts_per_page' => 1,
-					'include' => $prevPost->ID
-				);
-				$prevPost = get_posts( $args );
-				foreach ( $prevPost as $post ) {
-					setup_postdata( $post );
-					get_template_part( 'template-parts/content', 'thumbnail' );
-					wp_reset_postdata();
+				<?php
+				$prevPost = get_previous_post( true );
+				if ( $prevPost ) {
+					$args = array(
+						'posts_per_page' => 1,
+						'include' => $prevPost->ID
+					);
+					$prevPost = get_posts( $args );
+					foreach ( $prevPost as $post ) {
+						setup_postdata( $post );
+						get_template_part( 'template-parts/content', 'thumbnail' );
+						wp_reset_postdata();
+					}
 				}
-			}
 
-			$nextPost = get_next_post( true );
-			if( $nextPost ) {
-				$args = array(
-					'posts_per_page' => 1,
-					'include' => $nextPost->ID
-				);
-				$nextPost = get_posts( $args );
-				foreach ( $nextPost as $post ) {
-					setup_postdata( $post );
-					get_template_part( 'template-parts/content', 'thumbnail' );
-					wp_reset_postdata();
+				$nextPost = get_next_post( true );
+				if( $nextPost ) {
+					$args = array(
+						'posts_per_page' => 1,
+						'include' => $nextPost->ID
+					);
+					$nextPost = get_posts( $args );
+					foreach ( $nextPost as $post ) {
+						setup_postdata( $post );
+						get_template_part( 'template-parts/content', 'thumbnail' );
+						wp_reset_postdata();
+					}
 				}
-			}
-			?>
+				?>
+				<div class="column-spacer"></div>
 			</div>
 		</div>
 		<?php
--- template-parts/content-thumbnail.php
+++ template-parts/content-thumbnail.php
@@ -1,12 +1,14 @@
 <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-	<a href="<?php the_permalink(); ?>" rel="bookmark"<?php if ( has_post_thumbnail() ) { ?> style="background-image: url( '<?php echo get_the_post_thumbnail_url( get_the_ID(), 'custom-thumbnails' ) ?>' )"<?php } ?>>
-	<?php the_title( '<h3 class="entry-title">', ' <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="24" height="24"><g><path d="M10 20l8-8-8-8-1.414 1.414L15.172 12l-6.586 6.586"></path></g></svg></h3>' ); ?>
-	</a>
+	<section>
+		<a href="<?php the_permalink(); ?>" rel="bookmark"<?php if ( has_post_thumbnail() ) { ?> style="background-image: url( '<?php echo get_the_post_thumbnail_url( get_the_ID(), 'custom-thumbnails' ) ?>' )"<?php } ?>>
+		<?php the_title( '<h3 class="entry-title">', ' <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="24" height="24"><g><path d="M10 20l8-8-8-8-1.414 1.414L15.172 12l-6.586 6.586"></path></g></svg></h3>' ); ?>
+		</a>
 
-	<?php if ( 'post' === get_post_type() ) : ?>
-	<div class="entry-meta">
-		<?php navi_posted_on(); ?>
-	</div>
-	<?php
-	endif; ?>
+		<?php if ( 'post' === get_post_type() ) : ?>
+		<div class="entry-meta">
+			<?php navi_posted_on(); ?>
+		</div>
+		<?php
+		endif; ?>
+	</section>
 </article>
