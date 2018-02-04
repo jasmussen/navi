ed96d97789745b3c457fb7621748e9c39f8b4fa7
Joen Asmussen <asmussen@gmail.com>
2018-02-04 10:49:43 +0100

    Add center alignment
    Also polish search and 404

--- archive.php
+++ archive.php
@@ -1,13 +1,4 @@
-<?php
-/**
- * The template for displaying archive pages
- *
- * @link https://codex.wordpress.org/Template_Hierarchy
- *
- * @package navi
- */
-
-get_header(); ?>
+<?php get_header(); ?>
 
 	<div id="primary" class="content-area">
 		<main id="main" class="site-main">
@@ -23,14 +14,8 @@ get_header(); ?>
 			</header><!-- .page-header -->
 
 			<?php
-			/* Start the Loop */
 			while ( have_posts() ) : the_post();
 
-				/*
-				 * Include the Post-Format-specific template for the content.
-				 * If you want to override this in a child theme, then include a file
-				 * called content-___.php (where ___ is the Post Format name) and that will be used instead.
-				 */
 				get_template_part( 'template-parts/content', get_post_format() );
 
 			endwhile;
@@ -43,8 +28,8 @@ get_header(); ?>
 
 		endif; ?>
 
-		</main><!-- #main -->
-	</div><!-- #primary -->
+		</main>
+	</div>
 
 <?php
 get_footer();
--- search.php
+++ search.php
@@ -1,36 +1,15 @@
-<?php
-/**
- * The template for displaying search results pages
- *
- * @link https://developer.wordpress.org/themes/basics/template-hierarchy/#search-result
- *
- * @package navi
- */
-
-get_header(); ?>
-
-	<section id="primary" class="content-area">
-		<main id="main" class="site-main">
+<?php get_header(); ?>
 
 		<?php
 		if ( have_posts() ) : ?>
 
-			<header class="page-header">
-				<h1 class="page-title"><?php
-					/* translators: %s: search query. */
-					printf( esc_html__( 'Search Results for: %s', 'navi' ), '<span>' . get_search_query() . '</span>' );
-				?></h1>
-			</header><!-- .page-header -->
+			<header>
+				<h1 class="page-title"><?php printf( esc_html__( 'Search Results for: %s', 'navi' ), '<span>' . get_search_query() . '</span>' ); ?></h1>
+			</header>
 
 			<?php
-			/* Start the Loop */
 			while ( have_posts() ) : the_post();
 
-				/**
-				 * Run the loop for the search to output the results.
-				 * If you want to overload this in a child theme then include a file
-				 * called content-search.php and that will be used instead.
-				 */
 				get_template_part( 'template-parts/content', 'search' );
 
 			endwhile;
@@ -41,10 +20,6 @@ get_header(); ?>
 
 			get_template_part( 'template-parts/content', 'none' );
 
-		endif; ?>
+		endif;
 
-		</main><!-- #main -->
-	</section><!-- #primary -->
-
-<?php
 get_footer();
