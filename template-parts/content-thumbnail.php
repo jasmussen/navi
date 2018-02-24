de25fdfc59034338e288c96b965e04d1c18215f5
Joen Asmussen <asmussen@gmail.com>
2018-02-24 10:27:22 +0100

    Style tables, captions, tags.

--- template-parts/content-thumbnail.php
+++ template-parts/content-thumbnail.php
@@ -1,6 +1,6 @@
 <div <?php post_class( 'thumbnail' ); ?>>
 	<section>
-		<?php the_tags( '<p class="post-tags">', '', '</p>' ); ?>
+		<?php if ( !$title ) { ?><p class="post-tags"><?php the_tags( '', ', ', '' ); ?></p><?php } ?>
 		<?php if ( $title ) { echo '<h3>' . $title . '</h3>'; } ?>
 		<h4><a href="<?php the_permalink(); ?>" rel="bookmark"><?php the_title(); ?></a></h4>
 		<?php if ( has_excerpt() ) { the_excerpt(); } ?>
